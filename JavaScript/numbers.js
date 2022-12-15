069aa5d11504cd64b0097422aec40908a612ec96
henriquexpaulo <phvieira3@gmail.com>
2022-12-15 18:38:37 -0300

    atualização

--- JavaScript/numbers.js
+++ JavaScript/numbers.js
@@ -11,4 +11,5 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2
 const media = total / (peso1 + peso2)
 
 console.log(media.toFixed(2))
-console.log(media.toString())
+console.log(media.toString()) // em binario 
+console.log(typeof Number)
